f9fa0242c72e3f4a2f4fe7743e02bd2dc404bdd5
Murad <tuhum_05@mail.ru>
2024-03-23 01:12:57 +0700

    Изм. сигнально-слотовую связь

--- 2_9/win.cpp
+++ 2_9/win.cpp
@@ -1,12 +1,12 @@
 #include "win.h"
 Window::Window()
 {
-codec = QTextCodec::codecForName("Windows-1251");
-this->setWindowTitle(codec->toUnicode("Обработка событий"));
+this->setWindowTitle("Обработка событий");
 area = new Area( this );
-btn = new QPushButton(codec->toUnicode("Завершить"),this );
+btn = new QPushButton("Завершить", this);
 QVBoxLayout *layout = new QVBoxLayout(this);
 layout->addWidget(area);
 layout->addWidget(btn);
-connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+connect(btn, &QPushButton::clicked,
+        this, &Window::close);
 };
